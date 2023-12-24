2c69e61995e27d854c1a4b75b589d4d7bf15d2c1
Noel Quintos <npquintos@gmail.com>
2023-12-23 18:56:17 -0700

    Add files via upload

--- qreader.hpp
+++ qreader.hpp
@@ -1,22 +1,30 @@
-#include<stdio.h>
+#include<cstdio>
+#include<iostream>
 #include<stdexcept>
 #include<filesystem>
+#include<cstring>
+#include <limits.h>
 #define QMAXBUFFER 10240 // chosen for performance
 #define QLINEMAX 1024 // assumes that one line is less than 1023 chars
 
 class Qreader {
     private:
-        char buffer[QMAXBUFFER];
-        char line[QLINEMAX];
-        FILE *fin;
+        ulong line_count = 0;
+        ulong bytes_read = 0;
+        ulong line_start_byte_position_in_file;
+        ulong line_max = ULONG_MAX;
+        std::FILE *fin;
         int bufInd = 0;
         int bufRemaining = 0;
         int lineInd = 0;
-        long line_count = 0;
+        char buffer[QMAXBUFFER];
+        char last_char = 'n';
+        char line[QLINEMAX];
     public:
         bool eol = false;
+        // std::FILE *fin;
         Qreader(const char* filename) : eol(false) {
-            if(!(fin = fopen(filename, "r"))) {
+            if(!(fin = std::fopen(filename, "rb"))) {
                 exit(1);
             }
         };
@@ -25,47 +33,68 @@ class Qreader {
         Qreader(Qreader &&other) = delete;
         Qreader &operator=(const Qreader &other) = delete;
         Qreader &operator=(Qreader &&other) = delete;
-        ~Qreader() { fclose(fin); };
+        ~Qreader() { std::fclose(fin); };
 
+        ulong get_byte_offset() const {
+            return line_start_byte_position_in_file;
+        };
         // IMPORTANT: user should declare at his side "char out_buf[QLINEMAX];"
         bool read(char *out_buf) {
             int out_index = 0;
-            if (!bufRemaining) {
-                bufRemaining = fread(buffer, sizeof(char), QMAXBUFFER, fin);
+            while (!eol) {
                 if (!bufRemaining) {
-                    eol = true;
-                    return false;
+                    bufRemaining = fread(buffer, sizeof(char), QMAXBUFFER, fin);
+                    if (!bufRemaining) {
+                        eol = true;
+                        line_max = line_count;
+                        if (bufInd) { // there is still an unreturned line
+                            out_buf[out_index] = '\0';
+                            if (buffer[bufInd] == '\n') {
+
+                                line_start_byte_position_in_file = bytes_read - strlen(out_buf)-1;
+                                // std::cout << "returned from CR but eof" << std::endl;
+                            }
+                            else {
+                                line_start_byte_position_in_file = bytes_read - strlen(out_buf);
+                                // std::cout << "returned without CR but eof" << std::endl;
+                            }
+                        }
+                        return false;
+                    }
+                    bufInd = 0;
+                }
+                while(bufRemaining && buffer[bufInd] != '\n' && out_index < QLINEMAX-1) {
+                    out_buf[out_index++] = buffer[bufInd++];
+                    ++bytes_read;
+                    --bufRemaining;
+                }
+                if (bufRemaining) {
+                    --bufRemaining;
+                    ++bufInd;
+                    ++bytes_read;
+                    out_buf[out_index] = '\0';
+                    line_start_byte_position_in_file = bytes_read - 1 - strlen(out_buf);
+                    ++line_count;
+                    // std::cout << "returned from normal CR" << std::endl;
+                    return true;
                 }
             }
-            while(bufRemaining && buffer[bufInd] != '\n' && out_index < QLINEMAX-1) {
-                out_buf[out_index] = buffer[bufInd];
-                --bufRemaining;
-                ++bufInd;
-                ++out_index;
-            }
-            --bufRemaining;
-            ++bufInd;
-            out_buf[out_index] = '\0';
-            ++line_count;
-            return true;
+            eol = true;
+            return false;
         };
         
-        // offset is the number of lines from beginning
+        // offset is seek offset  is the number of lines from beginning
         void seek(const long offset) {
-            long end_line = offset - line_count;
-            if (end_line < 0) {
-                end_line = offset;
-                std::fseek(fin, 0, SEEK_SET);
-            }
-            for (int i=0; i<end_line; ++i) {
-                read(line);
-            }
-            line_count = offset;
-            eol = false;
+            std::fseek(fin, offset, SEEK_SET);
+            bufRemaining = 0;
         };
 
-        // this is the number of lines from the beginning
+        // this is the byte offset from the beginning of file
+        // for the currently read line. Take note that you read
+        // the line first to examine if this is the line you
+        // are looking for and so, you need to calculate backwards
+        // to determine the beginning of this line.
         long tell(void) const {
-            return line_count;
+            return line_start_byte_position_in_file;
         };
 };
--- test_linereader.cpp
+++ test_linereader.cpp
@@ -1,14 +1,16 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "../../include/qreader.hpp"
-#include "../../include/doctest.h"
-#include "string.h"
+#include "include/qreader.hpp"
+#include "include/doctest.h"
+#include <string.h>
+#include <limits.h>
+#include <string_view>
 
 TEST_CASE("Test one-time utility call") {
     char line[QLINEMAX];
     const char *correct[] = {"First line 111", "Second line 222", "Third line 333 "};
     const char *correct2[] = {"", "", "", "First line 111", "Second line 222", "", "", "Third line 333 ", "", "",};
     SUBCASE("######## Test a well-formed file ########") {
-        auto qr = Qreader("../data/test.txt");
+        auto qr = Qreader("../include/test.txt");
         int i = 0;
         while (qr.read(line)) {
             CHECK(!strcmp(line, correct[i]));
@@ -16,7 +18,7 @@ TEST_CASE("Test one-time utility call") {
         }
     }
     SUBCASE("######## Test a file with empty lines ########") {
-        auto qr = Qreader("../data/test2.txt");
+        auto qr = Qreader("../include/test2.txt");
         int i = 0;
         while (qr.read(line)) {
             CAPTURE(line);
@@ -24,5 +26,37 @@ TEST_CASE("Test one-time utility call") {
             CHECK(!strcmp(line, correct2[i]));
             ++i;
         }
+    }
+    SUBCASE("######## Test seek and tell functionality ########") {
+        auto qr = Qreader("../include/test3.txt");
+        int i = 0;
+        while (i < 1 && qr.read(line))
+            ++i;
+        long line1 = qr.tell();
+        std::cout << line1 << " line1: " << line << std::endl;
+        while (i < 12 && qr.read(line))
+            ++i;
+        long line12 = qr.tell();
+        std::cout << line12 << " line12: " << line << std::endl;
+        while (i < 45 && qr.read(line))
+            ++i;
+        long line45 = qr.tell();
+        std::cout << line45 << " line45: " << line << std::endl;
+        while (i < 50 && qr.read(line))
+            ++i;
+        long line50 = qr.tell();
+        std::cout << line50 << " line50: " << line << std::endl;
+        qr.seek(line1); 
+        qr.read(line);
+        CHECK(std::string_view("This is absolutely at line number 1") == line);
+        qr.seek(line12); 
+        qr.read(line);
+        CHECK(std::string_view("This is absolutely at line number 12") == line);
+        qr.seek(line45); 
+        qr.read(line);
+        CHECK(std::string_view("This is absolutely at line number 45") == line);
+        qr.seek(line50); 
+        qr.read(line);
+        CHECK(std::string_view("This is absolutely at line number 50") == line);
     }
 }
